38da13fbd1aeb9e5be213daa0118733ee347c94e
jimstir <jimmydebe0@gmail.com>
2024-10-19 20:25:31 -0400

    contracts_Update3

--- src/contracts/DataSharing.sol
+++ src/contracts/DataSharing.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.20;
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract PayLater {
+contract DataSharing {
 
     event withdrawal(uint256 amount, uint256 releaseTime);
 
@@ -33,6 +33,7 @@ contract PayLater {
     * @dev Set a price data order type ( can create more than one type)
     * MUST account for decimals
     */
+
     function setDataOrder() public virtual  {
         require(msg.sender == _owner);
 
@@ -41,6 +42,7 @@ contract PayLater {
     * @dev Make a purchase for a data order
     * return orderNum
     */
+
     function dataPurchase(IERC20 token, uint256 amount, uint256 dNum ) public virtual returns(uint256) {
         require(amount <= _orderAmount[dNum]);
         _orderNum = _orderNum + 1;
@@ -49,14 +51,16 @@ contract PayLater {
         _purchases[msg.sender][_orderNum] = true;
         return _orderNum;
     }
+
     /**
-    * @dev Withdraw funds(merchat only user allowed to withdraw)(profit sharing on-chain not implemented)
+    * @dev Withdraw funds(reserve only recieves funds)(profit sharing on-chain not implemented)
     * MUST account for decimals
     */
+
     function withdrawFunds(IERC20 token, uint256 amount ) public virtual {
         require(msg.sender == _owner);
 
-        SafeERC20.safeTransferFrom(IERC20(token),address(this), msg.sender, amount);
+        SafeERC20.safeTransferFrom(IERC20(token),address(this), _reserve, amount);
 
         emit withdrawal(amount, block.timestamp);
     }
